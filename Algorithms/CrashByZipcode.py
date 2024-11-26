be24fb8e123c1117813b5227e16179d61f169af3
alexlopez7498 <130875480+alexlopez7498@users.noreply.github.com>
2024-11-25 19:29:21 -0800

    Added bar graphs to the rest of the algorithms

--- Algorithms/CrashByZipcode.py
+++ Algorithms/CrashByZipcode.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession, functions as F
-
+import matplotlib.pyplot as plt
 # Create Spark session
 spark = SparkSession.builder.appName("CrashAnalysis").getOrCreate()
 
@@ -12,9 +12,29 @@ zipcode_crashes = df.groupBy("ZIP CODE").agg(F.count("*").alias("CRASH_COUNT"))
 
 # Sort by CRASH_COUNT in descending order
 zipcode_crashes = zipcode_crashes.orderBy(F.desc("CRASH_COUNT"))
+results = zipcode_crashes.collect()
 
-# Write the result to a new CSV file
-zipcode_output_path = "zipcode_crash_counts.csv"
-zipcode_crashes.write.mode("overwrite").option("header", "true").csv(zipcode_output_path)
+Zipcodes = [row['ZIP CODE'] for row in results]
+crash_counts = [row['CRASH_COUNT'] for row in results]
+
+plt.ion()
+plt.figure(figsize=(16, 10))  # Increase the width and height
+
+    # Create the bar plot
+bar_width = 0.8  # Adjust this value as needed (default is 0.8)
+plt.bar(Zipcodes[:100], crash_counts[:100], color='blue', width=bar_width)
+plt.xlabel('ZIP CODE')
+plt.ylabel('Crash Count')
+plt.title('Crash Count by Vehicle Year')
+plt.xticks(ticks=range(len(Zipcodes[:100])), labels=Zipcodes[:100], rotation=90, ha='right')
+
+
+plt.ylim(0, 600000)
+plt.tight_layout()
+    # Show the plot and allow interaction
+plt.show(block=False)
+
+    # Keep the plot window open
+plt.show(block=True)
 # Show the result for verification
 zipcode_crashes.show(truncate=False)
